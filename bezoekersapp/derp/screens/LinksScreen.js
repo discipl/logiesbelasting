1da1fa6d83619dbe16b0d991fb678bd651ba3fa5
Willgo97 <williamgoudswaard@gmail.com>
2018-03-28 10:23:43 +0200

    test bezoekersapp toegevoegd

---
+++ bezoekersapp/derp/screens/LinksScreen.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import StarRating from 'react-native-star-rating';
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      starCount: 3
+    };
+  }
+
+  onStarRatingPress(rating) {
+    this.setState({
+      starCount: rating
+    });
+  }
+
+  render() {
+    return (
+      <StarRating
+        disabled={false}
+        emptyStar={'ios-star-outline'}
+        fullStar={'ios-star'}
+        halfStar={'ios-star-half'}
+        iconSet={'Ionicons'}
+        maxStars={5}
+        rating={this.state.starCount}
+        selectedStar={(rating) => this.onStarRatingPress(rating)}
+        fullStarColor={'gold'}
+      />
+    );
+  }
+}
