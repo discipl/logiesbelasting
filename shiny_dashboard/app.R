2a998774fb1852adbb79c1ee4d99039932bd4489
Menno van Rijsselberg <0919433@hr.nl>
2018-06-13 11:37:07 +0200

    Changed dashboard language to Dutch

--- shiny_dashboard/app.R
+++ shiny_dashboard/app.R
@@ -5,7 +5,7 @@ library(DT)
 
 ui <- fluidPage(
   tabsetPanel(
-    tabPanel("Activiteit",
+    tabPanel("Populair",
       sidebarLayout(
         sidebarPanel(tags$h1("Meest bezochte events"), br(), br(),
           sliderInput(inputId = "num",
@@ -19,10 +19,10 @@ ui <- fluidPage(
         )
       )
     ),
-    tabPanel("Populair",
+    tabPanel("Activiteit",
       sidebarLayout(
-        sidebarPanel(tags$h1("Events until last transaction"), br(), br(),
-          actionButton("reloadButton2", "Reload XML file")
+        sidebarPanel(tags$h1("Events tot laatste transactie"), br(), br(),
+          actionButton("reloadButton2", "Haal data opnieuw op")
         ),
         mainPanel(
           height = 8,
@@ -42,21 +42,21 @@ numbins <- cut(newdataframepop, 20)
 
 server <- function(input, output, session) {
     observeEvent(eventExpr = input$reloadButton, {
-        showNotification("Reloading XML file, this might take a while")
+        showNotification("Herladen van XML file, dit kan even duren")
         xmldataframe <- xmlToDataFrame("dashboard.xml")
         newdataframe <- as.data.frame(table(xmldataframe$event))
         newdataframe2 <- newdataframe[order(- newdataframe$Freq),]
-        showNotification("Updated XML file!")
+        showNotification("XML file geupdated!")
         output$eventbarplot <- renderPlot({
             barplot(newdataframe2$Freq, names.arg = newdataframe2$Var1, xlim = c(0, input$num), ylim = c(0, max(newdataframe2$Freq, na.rm = TRUE)), las = 2, col = "red")
         })
     })
     observeEvent(eventExpr = input$reloadButton2, {
-        showNotification("Reloading XML file, this might take a while")
+        showNotification("Herladen van XML file, dit kan even duren")
         xmldataframe <- xmlToDataFrame("dashboard.xml")
-        newdataframe <- as.numeric(as.character(xmldataframe[["timestamp"]]))
-        numbins <- cut(newdataframe, 20)
-        showNotification("Updated XML file!")
+        newdataframepop <- as.numeric(as.character(xmldataframe[["timestamp"]]))
+        numbins <- cut(newdataframepop, 20)
+        showNotification("XML file geupdated!")
         output$eventgraph <- renderPlot({
             plot(numbins, xaxt = 'n', col = "blue")
         })
@@ -65,7 +65,7 @@ server <- function(input, output, session) {
         barplot(newdataframe2$Freq, names.arg = newdataframe2$Var1, xlim = c(0, input$num), ylim = c(0, max(newdataframe2$Freq, na.rm = TRUE)), las = 2, col = "red")
     })
     output$eventgraph <- renderPlot({
-        plot(numbins, xaxt = 'n', col = "blue")
+        plot(numbins, xaxt = 'n', xlab = paste("Transacties van ", as.POSIXct(as.numeric(as.character(min(newdataframepop / 1000))),origin="1970-01-01",tz="CET"), " tot ", as.POSIXct(as.numeric(as.character(max(newdataframepop / 1000))),origin="1970-01-01",tz="CET")), col = "blue")
     })
 }
 
